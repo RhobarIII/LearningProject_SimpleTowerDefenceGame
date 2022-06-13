c203aefa2b882aa5cd80b46f75e5a8a90989e88c
RhobarIII <b.k.zygmunt@gmail.com>
2022-06-13 17:02:18 +0200

    SimpleCastleUI

--- SimpleTowerDefence/Assets/_Udemy Tower Defence Assets/Scripts/CastleController.cs
+++ SimpleTowerDefence/Assets/_Udemy Tower Defence Assets/Scripts/CastleController.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CastleController : MonoBehaviour
 {
     [SerializeField] float castleHealth = 100f;
+
+    public Slider healthSilder;
+    public Text healthValue;
+    
+
     private float currentCastelHealth;
+   
     void Start()
     {
+        
         currentCastelHealth = castleHealth;
+        healthSilder.maxValue = castleHealth;
+        healthSilder.value = currentCastelHealth;
+         healthValue.text = castleHealth.ToString()+" / "+castleHealth.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+       
         
     }
     public void TakeDamage(float damageToTake)
     {
         currentCastelHealth -= damageToTake;
-        if(currentCastelHealth<=0)
+        healthSilder.value = currentCastelHealth;
+        healthValue.text = currentCastelHealth.ToString() + " / " + castleHealth.ToString();
+        if (currentCastelHealth<=0)
         {
             gameObject.SetActive(false);
         }
+        
     }
 }
